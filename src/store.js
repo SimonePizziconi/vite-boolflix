298826497ef9ba18fecd3c1247a64564b190d022
SimonePizziconi <simonepizziconi05@gmail.com>
2024-07-11 13:16:46 +0200

    feat: create store and methods for call api

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { reactive } from "vue"
+
+export const store = reactive ({
+    // Array da popolare con api ed il suo URL
+    movieList: [],
+    apiURL: "https://api.themoviedb.org/3/search/movie?api_key=2ad3d2dedcef1aaf835ebff23ac5b7de&query",
+
+});
